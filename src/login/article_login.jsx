3f8828d9731ded5991ebd3ea4f016d18dd3cc174
Jose Amado Ferrer Varela <itsesistemasdigitales@gmail.com>
2024-01-25 22:14:35 -0600

    version beta

--- src/login/article_login.jsx
+++ src/login/article_login.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import {useState} from 'react'
 
 export function Article_Login() {
-   
+   const [message, setMessage] = useState("")
   const IDBRequest= indexedDB.open("mibaserose", 1);
 
   IDBRequest.addEventListener("upgradeneeded",()=>{
@@ -19,13 +19,13 @@ export function Article_Login() {
   console.log("ocurrio un error creando la base de datos")
   })
   
-  
+
   const checkLocalStorage=e=>{
         e.preventDefault();
       
         const datos= new FormData(e.target);
         
-        
+
         const db=IDBRequest.result;
         const IDBtransaction = db.transaction("users","readonly");
         const objectStore=IDBtransaction.objectStore("users");
@@ -33,27 +33,31 @@ export function Article_Login() {
         cursor.addEventListener("success",()=>{
 
           if (cursor.result) {
-            console.log(cursor.result.value)
+            // console.log(cursor.result.value)
             if (datos.get("email") == cursor.result.value.email && datos.get("password")== cursor.result.value.password) {
               sessionStorage.setItem("email", datos.get("email"));
               location.href='#/'    
-            }
+            }  
             
-            cursor.result.continue()
+            cursor.result.continue() 
+          }else{
+             setMessage("los datos que introduciste son incorrectos")
+         }     
 
-            
-          }
         })
      
      }
      
        return (
          <div >
-         <h3>ingresa tus datos</h3>
+         <h3>ingresa tus datos para iniciar sesion</h3>
          <form id="formulario" onSubmit={checkLocalStorage}>
            <input name="email" type="text" />
-           <input name="password" type="password" />
-           <input type="submit"/>
+           <input name="password" type="password"/>
+           <input type="submit" value="iniciar"/>
+           <br />
+           <br />
+           <small>{message}</small>
          </form>
          </div>
        )
--- src/submit/article_submit.jsx
+++ src/submit/article_submit.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 
 export function Article_Submit() {
 
         const IDBRequest= indexedDB.open("mibaserose", 1);
-
+        
         IDBRequest.addEventListener("upgradeneeded",()=>{
           const db=IDBRequest.result;
           db.createObjectStore("users",{
@@ -13,14 +15,43 @@ export function Article_Submit() {
         
         IDBRequest.addEventListener("success",()=>{
           console.log("todo salio correctamente");
+          
         })
 
         IDBRequest.addEventListener("error",()=>{
         console.log("ocurrio un error creando la base de datos")
         })
+
         
-   
-        const saveLocalStorage= e =>{
+        // const initialValues={
+        //   email:'',
+        //   password :''
+        // }
+
+        // const validationSchema=Yup.object({
+        //   email:Yup.string().email("debes introducir un email valido").required("el campo es requerido"),
+        //   password:Yup.string().min(5,"minimo 5 caracterres").max(10,"maximo 10 caracteres").required("el campo es requerido")
+        // })
+        
+        // const onSubmit=  (values)=>{
+          
+        //     const data ={email:values.email,password:values.password}
+        //     const db= IDBRequest.result;
+        //     const IDBtransaction = db.transaction("users","readwrite");
+        //     const objectStore=IDBtransaction.objectStore("users");
+        //     objectStore.add(data);
+        //     IDBtransaction.addEventListener("complete",()=>{
+        //     location.href='#/login' 
+        //     })
+        //   }
+
+        // const {handleChange, errors, handleSubmit}=useFormik({
+        //   initialValues,
+        //   onSubmit,
+        //   validationSchema
+        // })   
+
+        const saveLocalStorage = e =>{
         e.preventDefault();
         const datos= new FormData(e.target);        
         const data ={email:datos.get("email"),password:datos.get("password")}
@@ -29,18 +60,21 @@ export function Article_Submit() {
         const objectStore=IDBtransaction.objectStore("users");
         objectStore.add(data)
         IDBtransaction.addEventListener("complete",()=>{
-         console.log("objeto agregado correctamente") 
+         location.href='#/login' 
+         
         })
 
         }
      
        return (
          <div >
-         <h3>ingresa tus datos</h3>
+         <h3>ingresa tus datos para crear una cuenta</h3>
          <form id="formulario" onSubmit={saveLocalStorage}>
            <input name="email" type="text" />
-           <input name="password" type="password" />
-           <input type="submit"/>
+           {/* <small>{errors?.email}</small> */}
+           <input name="password" type="password"  />
+           {/* <small>{errors?.password}</small><br /> <br /> */}
+           <input type="submit" value="crear"/>
          </form>
          </div>
        )
